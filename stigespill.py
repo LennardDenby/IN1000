e1339e77fcc0d6dfd253b0533b85fecde9b5288d
lennarddenby <lennard.rolstad.denby@gmail.com>
2024-11-04 14:59:36 +0100

    -

--- stigespill.py
+++ stigespill.py
@@ -3,29 +3,25 @@ def stigespill(terningkast: list[int], stiger: dict[int, int]) -> int:
     
     for tall in terningkast:
         posisjon += tall
+        
         if posisjon in stiger:
             posisjon = stiger[posisjon]
     
     return posisjon
 
 def hvilke_tre_kast(slutt_rute: int, stiger: dict[int, int]) -> list[list[int]]:
-    mulige_kast = []
+    returListe = []
     
     for i in range(1, 7):
         for j in range(1, 7):
             for k in range(1, 7):
                 if stigespill([i, j, k], stiger) == slutt_rute:
-                    mulige_kast.append([i, j, k])
+                    returListe.append([i, j, k])
     
-    return mulige_kast
-                    
+    return returListe
 
-def tester():
-    assert stigespill([5,4,2,2],{5:12, 18:7}) == 9
-    assert hvilke_tre_kast(5, {3:15, 17:4}) == [[1, 1, 3], [1, 3, 1], [2, 2, 1], [3, 2, 1]]
-    
 def main():
-    tester()
+    print(hvilke_tre_kast(5, {3:15, 17:4}) )
 
 if __name__ == '__main__':
     main()
